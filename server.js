e4322c95d6bacf29af0a4454f294e3f0876f356b
Kasey Knudsen <kaseyknudsen@gmail.com>
2022-12-08 13:49:36 -0800

    Intro to express part 2

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const morgan = require("morgan")
 
 const hostname = "localhost";
 const port = 3000;
 
 //this returns an express server application thats now stored in app
 const app = express();
+//this will configure morgan to use the development version which will print more info to the screen
+app.use(morgan('dev'))
+
+/* set up express to serve static files from the public folder. With express.static, we can do this
+ in a single line*/
+//__dirname gives us the absolute path
+app.use(express.static(__dirname + '/public'))
 
 /*  use takes a callback function which is called a middleware function. It takes 3 parameters
 1) req
@@ -12,7 +20,6 @@ const app = express();
 3) next (function)
 */
 app.use((req, res) => {
-  console.log(req.headers);
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
   res.end("<html><body><h1>This is an Express Server</h1></body></html>");
@@ -22,3 +29,4 @@ app.use((req, res) => {
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:{port}/`);
 });
+
