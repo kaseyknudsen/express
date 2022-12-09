f79b06f199578199f84ecf33fb65cf0a1bb62252
Kasey Knudsen <kaseyknudsen@gmail.com>
2022-12-09 10:22:39 -0800

    express simple REST

--- server.js
+++ server.js
@@ -1,32 +1,63 @@
-const express = require("express");
-const morgan = require("morgan")
+const express = require('express');
+const morgan = require('morgan');
 
-const hostname = "localhost";
+const hostname = 'localhost';
 const port = 3000;
 
-//this returns an express server application thats now stored in app
 const app = express();
-//this will configure morgan to use the development version which will print more info to the screen
-app.use(morgan('dev'))
+app.use(morgan('dev'));
+app.use(express.json());
 
-/* set up express to serve static files from the public folder. With express.static, we can do this
- in a single line*/
-//__dirname gives us the absolute path
-app.use(express.static(__dirname + '/public'))
+app.all('/campsites', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+});
 
-/*  use takes a callback function which is called a middleware function. It takes 3 parameters
-1) req
-2) res
-3) next (function)
-*/
+app.get('/campsites', (req, res) => {
+    res.end('Will send all the campsites to you');
+});
+
+app.post('/campsites', (req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+});
+
+app.put('/campsites', (req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
+});
+
+app.delete('/campsites', (req, res) => {
+    res.end('Deleting all campsites');
+});
+
+app.get('/campsites/:campsiteId', (req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+});
+
+app.post('/campsites/:campsiteId', (req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+});
+
+app.put('/campsites/:campsiteId', (req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name}
+        with description: ${req.body.description}`);
+});
+
+app.delete('/campsites/:campsiteId', (req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+});
+
+app.use(express.static(__dirname + '/public'));
+
 app.use((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end("<html><body><h1>This is an Express Server</h1></body></html>");
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 });
 
-//creates an instance of the http server class and starts listening to it
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:{port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
-
