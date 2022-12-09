c6c8e26cb10a214a7ff203052f0b57db9f50efbc
Kasey Knudsen <kaseyknudsen@gmail.com>
2022-12-09 10:41:49 -0800

    express router

---
+++ routes/campsiteRouter.js
@@ -0,0 +1,31 @@
+/* this file will contain the code for handling the REST API endpoints for campsites and 
+campsites/campsitesId */
+//we chained them to be one single statement by removing .app and the path from the parameters
+
+const express = require("express");
+const campsiteRouter = express.Router();
+
+campsiteRouter
+  .route("/")
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next();
+  })
+  .get((req, res) => {
+    res.end("Will send all the campsites to you");
+  })
+  .post((req, res) => {
+    res.end(
+      `Will add the campsite: ${req.body.name} with description: ${req.body.description}`
+    );
+  })
+  .put((req, res) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /campsites");
+  })
+  .delete((req, res) => {
+    res.end("Deleting all campsites");
+  });
+
+module.exports = campsiteRouter;
